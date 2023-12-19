36068cbe1527019e9a6c3c68d8fa4ffda54f54d9
Kat Morgan <kat@braincraft.io>
2023-12-19 04:54:58 +0000

    add nginx ingress helm chart deployment

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import pulumi
 import pulumi_kubernetes as k8s
+from pulumi_kubernetes.helm.v3 import Chart, ChartOpts, FetchOpts
 from src.konductor.pod import create_konductor_deployment
 
 # Configuration for the Kubernetes provider
@@ -16,6 +17,44 @@ konductor_deployment = create_konductor_deployment(
     image=image_name,
     replicas=replica_count,
     k8s_provider=k8s_provider
+)
+
+# Create a Kubernetes Namespace named 'ingress-nginx'
+nginx_namespace = k8s.core.v1.Namespace("ingress-nginx",
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        name="ingress-nginx",
+    )
+)
+
+# Install the NGINX Ingress Controller using a Helm Chart.
+nginx_ingress = Chart(
+    'nginx-ingress',
+    config=ChartOpts(
+        chart='ingress-nginx',
+        version='4.0.6',
+        namespace=nginx_namespace.metadata.name,
+        fetch_opts=FetchOpts(
+            repo='https://kubernetes.github.io/ingress-nginx'
+        ),
+        values={
+            'controller': {
+                'service': {
+                    'type': 'NodePort',
+                    'nodePorts': {
+                        'http': 30080,
+                        'https': 30443,
+                    },
+                    'externalTrafficPolicy': 'Local',
+                },
+            },
+        },
+    ),
+    opts=pulumi.ResourceOptions(
+        provider=k8s_provider,
+        depends_on=[
+            nginx_namespace
+        ]
+    )
 )
 
 # Export deployment details
